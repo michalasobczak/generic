684219d596524e257537b1a6863090e22c21ae95
Michal Sobczak <michalasobczak@gmail.com>
2018-05-30 16:07:35 +0200

    spaces instead of tabs

--- js/elements/vector.js
+++ js/elements/vector.js
@@ -1,139 +1,139 @@
 Vector3 = function(_x,_y,_z) {
-	return {
-		x: parseFloat(_x),
-		y: parseFloat(_y),
-		z: parseFloat(_z),
-		size: 1,
-		color: " #000000",
-		getAsSylv: function() {
-			return $V([this.x, this.y, this.z]);
-		},
-		normalize: function() {			
-			var result = this.getAsSylv().toUnitVector();			
-			this.x = result.e(1);
-			this.y = result.e(2);
-			this.z = result.e(3);
+  return {
+    x: parseFloat(_x),
+    y: parseFloat(_y),
+    z: parseFloat(_z),
+    size: 1,
+    color: " #000000",
+    getAsSylv: function() {
+      return $V([this.x, this.y, this.z]);
+    },
+    normalize: function() {      
+      var result = this.getAsSylv().toUnitVector();      
+      this.x = result.e(1);
+      this.y = result.e(2);
+      this.z = result.e(3);
 
-			return this;
-		},
-		rotate: function(th, linePoint, lineDirection) {			
-			var dir = this.getAsSylv().rotate(th, 
-										$L(
-										   $V([linePoint.x,linePoint.y,linePoint.z]),
-										   $V([lineDirection.x,lineDirection.y,lineDirection.z])
-										  ) 
-									    );
-			var r = Vector3(dir.e(1), dir.e(2), dir.e(3));			
-			return r;
-		},
-		dup: function() {
-			var r = Vector3(this.x, this.y, this.z);
-			return r;
-		},
-		multiply: function(val) {
-			var r = Vector3(this.x*val, this.y*val, this.z*val);
-			return r;
-		},
-		mul: function(val) {
-			var r = Vector3(this.x*val, this.y*val, this.z*val);
-			return r;
-		},
-		div: function(val) {
-			var r = Vector3(this.x/val, this.y/val, this.z/val);
-			return r;
-		},
-		inc: function(v) {
-			this.x += v.x;
-			this.y += v.y;
-			this.z += v.z;
-		},
-		dec: function(v) {
-			this.x -= v.x;
-			this.y -= v.y;
-			this.z -= v.z;
-		},
-		add: function(v) {
-			var r = Vector3(this.x+v.x, this.y+v.y, this.z+v.z);
-			return r;
-		},
-		sub: function(v) {
-			var r = Vector3(this.x-v.x, this.y-v.y, this.z-v.z);
-			return r;
-		},		
-		len: function() {
-			var r = this.getAsSylv().modulus();
-			return r;
-		},
-		ang: function(v) {
-			var r = this.getAsSylv().angleFrom(v.getAsSylv());
-			return r;
-		},
-		inv: function() {
-			var x = this.z;
-			var z = this.x;
-			var r = Vector3(x,this.y,z);
-			return r;
-		},
-		sig: function() {
-			var r = this.x + ":" + this.z;
-			return r;
-		},
-		toString: function() {
-			var r = this.x + "/" + this.y + "/" + this.z;
-			return r;
-		},
-		get2D: function() {			
-			var r = transform3Into2_NEW(this.x,this.y,this.z, camera.position.x,camera.position.y,camera.position.z, camera.angle);
-			return r;
-		},
-		draw: function(size) {
-			var S = transform3Into2_NEW(this);
-			ctx.fillStyle = this.color;
-			ctx.fillRect(S.x, S.y, size, size);
-		}
-	};
+      return this;
+    },
+    rotate: function(th, linePoint, lineDirection) {      
+      var dir = this.getAsSylv().rotate(th, 
+                    $L(
+                       $V([linePoint.x,linePoint.y,linePoint.z]),
+                       $V([lineDirection.x,lineDirection.y,lineDirection.z])
+                      ) 
+                      );
+      var r = Vector3(dir.e(1), dir.e(2), dir.e(3));      
+      return r;
+    },
+    dup: function() {
+      var r = Vector3(this.x, this.y, this.z);
+      return r;
+    },
+    multiply: function(val) {
+      var r = Vector3(this.x*val, this.y*val, this.z*val);
+      return r;
+    },
+    mul: function(val) {
+      var r = Vector3(this.x*val, this.y*val, this.z*val);
+      return r;
+    },
+    div: function(val) {
+      var r = Vector3(this.x/val, this.y/val, this.z/val);
+      return r;
+    },
+    inc: function(v) {
+      this.x += v.x;
+      this.y += v.y;
+      this.z += v.z;
+    },
+    dec: function(v) {
+      this.x -= v.x;
+      this.y -= v.y;
+      this.z -= v.z;
+    },
+    add: function(v) {
+      var r = Vector3(this.x+v.x, this.y+v.y, this.z+v.z);
+      return r;
+    },
+    sub: function(v) {
+      var r = Vector3(this.x-v.x, this.y-v.y, this.z-v.z);
+      return r;
+    },    
+    len: function() {
+      var r = this.getAsSylv().modulus();
+      return r;
+    },
+    ang: function(v) {
+      var r = this.getAsSylv().angleFrom(v.getAsSylv());
+      return r;
+    },
+    inv: function() {
+      var x = this.z;
+      var z = this.x;
+      var r = Vector3(x,this.y,z);
+      return r;
+    },
+    sig: function() {
+      var r = this.x + ":" + this.z;
+      return r;
+    },
+    toString: function() {
+      var r = this.x + "/" + this.y + "/" + this.z;
+      return r;
+    },
+    get2D: function() {      
+      var r = transform3Into2_NEW(this.x,this.y,this.z, camera.position.x,camera.position.y,camera.position.z, camera.angle);
+      return r;
+    },
+    draw: function(size) {
+      var S = transform3Into2_NEW(this);
+      ctx.fillStyle = this.color;
+      ctx.fillRect(S.x, S.y, size, size);
+    }
+  };
 };
 
 
 Vector2 = function(_x,_y) {
-	return {
-		x: _x,
-		y: _y,
-		dup: function() {
-			var r = Vector2(this.x, this.y);
-			return r;
-		},
-		sub: function(v) {
-			var r = Vector2(this.x-v.x, this.y-v.y);
-			return r;
-		},
-		div: function(val) {
-			var r = Vector2(this.x/val, this.y/val);
-			return r;
-		},
-		inc: function(v) {
-			this.x += v.x;
-			this.y += v.y;			
-		},
-		dec: function(v) {
-			this.x -= v.x;
-			this.y -= v.y;			
-		},
-		mul: function(val) {
-			var r = Vector2(this.x*val, this.y*val);
-			return r;
-		},
-		draw: function(size, color) {
-			ctx.fillStyle = color;
-			ctx.fillRect(this.x, this.y, size, size);
-		},		
-		drawCtx: function(_ctx_, size, color) {
-			_ctx_.fillStyle = color;
-			_ctx_.fillRect(this.x, this.y, size, size);
-		},
-		toString: function() {
-			var r = this.x + "/" + this.y;
-			return r;
-		}
-	};
+  return {
+    x: _x,
+    y: _y,
+    dup: function() {
+      var r = Vector2(this.x, this.y);
+      return r;
+    },
+    sub: function(v) {
+      var r = Vector2(this.x-v.x, this.y-v.y);
+      return r;
+    },
+    div: function(val) {
+      var r = Vector2(this.x/val, this.y/val);
+      return r;
+    },
+    inc: function(v) {
+      this.x += v.x;
+      this.y += v.y;      
+    },
+    dec: function(v) {
+      this.x -= v.x;
+      this.y -= v.y;      
+    },
+    mul: function(val) {
+      var r = Vector2(this.x*val, this.y*val);
+      return r;
+    },
+    draw: function(size, color) {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, size, size);
+    },    
+    drawCtx: function(_ctx_, size, color) {
+      _ctx_.fillStyle = color;
+      _ctx_.fillRect(this.x, this.y, size, size);
+    },
+    toString: function() {
+      var r = this.x + "/" + this.y;
+      return r;
+    }
+  };
 };
--- js/env/3d.js
+++ js/env/3d.js
@@ -2,139 +2,138 @@
 
 
 matM = function(a,b) {
-	var p1,p2,p3,p4,p5,p6,p7,p8,p9 = 0.0;
+  var p1,p2,p3,p4,p5,p6,p7,p8,p9 = 0.0;
+  p1 = a[0]*b[0] + a[1]*b[3] + a[2]*b[6];
+  p2 = a[0]*b[1] + a[1]*b[4] + a[2]*b[7];
+  p3 = a[0]*b[2] + a[1]*b[5] + a[2]*b[8];
+  
+  p4 = a[3]*b[0] + a[4]*b[3] + a[5]*b[6];
+  p5 = a[3]*b[1] + a[4]*b[4] + a[5]*b[7];
+  p6 = a[3]*b[2] + a[4]*b[5] + a[5]*b[8];
 
-	p1 = a[0]*b[0] + a[1]*b[3] + a[2]*b[6];
- 	p2 = a[0]*b[1] + a[1]*b[4] + a[2]*b[7];
- 	p3 = a[0]*b[2] + a[1]*b[5] + a[2]*b[8];
+  p7 = a[6]*b[0] + a[7]*b[3] + a[8]*b[6];
+  p8 = a[6]*b[1] + a[7]*b[4] + a[8]*b[7];
+  p9 = a[6]*b[2] + a[7]*b[5] + a[8]*b[8];
 
-	p4 = a[3]*b[0] + a[4]*b[3] + a[5]*b[6];
- 	p5 = a[3]*b[1] + a[4]*b[4] + a[5]*b[7];
- 	p6 = a[3]*b[2] + a[4]*b[5] + a[5]*b[8];
-
-	p7 = a[6]*b[0] + a[7]*b[3] + a[8]*b[6];
- 	p8 = a[6]*b[1] + a[7]*b[4] + a[8]*b[7];
- 	p9 = a[6]*b[2] + a[7]*b[5] + a[8]*b[8];
-
-	return [
-		p1,p2,p3,
-		p4,p5,p6,
-		p7,p8,p9
-	];
+  return [
+    p1,p2,p3,
+    p4,p5,p6,
+    p7,p8,p9
+  ];
 }
 
 
 matMV = function(a,b) {
-	var p1,p2,p3 = 0.0;
+  var p1,p2,p3 = 0.0;
 
-	var p1 = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
-	var p2 = a[3]*b[0] + a[4]*b[1] + a[5]*b[2];
-	var p3 = a[6]*b[0] + a[7]*b[1] + a[8]*b[2];
+  var p1 = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+  var p2 = a[3]*b[0] + a[4]*b[1] + a[5]*b[2];
+  var p3 = a[6]*b[0] + a[7]*b[1] + a[8]*b[2];
 
-	return [p1,p2,p3];
+  return [p1,p2,p3];
 }
 
 
 matS = function(a,b) {
-	return [
-		a[0]-b[0], a[1]-b[1], a[2]-b[2],
-		a[3]-b[3], a[4]-b[4], a[5]-b[5],
-		a[6]-b[6], a[7]-b[7], a[8]-b[8]
-	];
+  return [
+    a[0]-b[0], a[1]-b[1], a[2]-b[2],
+    a[3]-b[3], a[4]-b[4], a[5]-b[5],
+    a[6]-b[6], a[7]-b[7], a[8]-b[8]
+  ];
 }
 
 
 cacheB = {};
 getB = function(dx,dy,dz) {
-	var lookup = ""+dx+dy+dz;
-	var existingEntry = cacheB[lookup];
-	if (existingEntry !== undefined) {
-		return existingEntry;
-	}
-	else {
-		var newEntry = Vector2(0,0);
+  var lookup = ""+dx+dy+dz;
+  var existingEntry = cacheB[lookup];
+  if (existingEntry !== undefined) {
+    return existingEntry;
+  }
+  else {
+    var newEntry = Vector2(0,0);
     var ezDivDz = -700 / dz;
-		newEntry.x = -1*(dx*ezDivDz) + canvasWidthHalf;
-		newEntry.y = dy*ezDivDz;
-		cacheB[lookup] = newEntry;
-		return newEntry;
-	}
+    newEntry.x = -1*(dx*ezDivDz) + canvasWidthHalf;
+    newEntry.y = dy*ezDivDz;
+    cacheB[lookup] = newEntry;
+    return newEntry;
+  }
 }
 
 cacheT = {};
 getT = function(f, val) {
-	var lookup = f+val;
-	var existingEntry = cacheT[lookup];
-	if (existingEntry !== undefined) {
-		return existingEntry;
-	}
-	else {
-		var newEntry;
-		if (f == "sin") {
-			newEntry = Math.sin(val);
-		}
-		else if (f == "cos") {
-			newEntry = Math.cos(val);
-		}
-		cacheT[lookup] = newEntry;
-		return newEntry;
-	}	
+  var lookup = f+val;
+  var existingEntry = cacheT[lookup];
+  if (existingEntry !== undefined) {
+    return existingEntry;
+  }
+  else {
+    var newEntry;
+    if (f == "sin") {
+      newEntry = Math.sin(val);
+    }
+    else if (f == "cos") {
+      newEntry = Math.cos(val);
+    }
+    cacheT[lookup] = newEntry;
+    return newEntry;
+  }  
 }
 
 cacheM = {};
 getM = function(th) {
-	var lookup = ""+th.x+th.y+th.z;
-	var existingEntry = cacheM[lookup];
-	if (existingEntry !== undefined) {
-		return existingEntry;
-	}
-	else {
-		var d1 = [
-			1, 0,                0                  ,
-			0, getT("cos",th.x), -1*getT("sin",th.x),
-			0, getT("sin",th.x), getT("cos",th.x)   ];
-		var d2 = [
-			getT("cos",th.y),    0, getT("sin",th.y),
-			0,                   1, 0               ,
-			-1*getT("sin",th.y), 0, getT("cos",th.y)];
-		var d3 = [
-			getT("cos",th.z), -1*getT("sin",th.z), 0,
-			getT("sin",th.z), getT("cos",th.z),    0,
-			0,                0,                   1];
+  var lookup = ""+th.x+th.y+th.z;
+  var existingEntry = cacheM[lookup];
+  if (existingEntry !== undefined) {
+    return existingEntry;
+  }
+  else {
+    var d1 = [
+      1, 0,                0                  ,
+      0, getT("cos",th.x), -1*getT("sin",th.x),
+      0, getT("sin",th.x), getT("cos",th.x)   ];
+    var d2 = [
+      getT("cos",th.y),    0, getT("sin",th.y),
+      0,                   1, 0               ,
+      -1*getT("sin",th.y), 0, getT("cos",th.y)];
+    var d3 = [
+      getT("cos",th.z), -1*getT("sin",th.z), 0,
+      getT("sin",th.z), getT("cos",th.z),    0,
+      0,                0,                   1];
 
-		var d1d2 = matM(d1,d2);	
-		var newEntry = matM(d1d2,d3);
+    var d1d2 = matM(d1,d2);  
+    var newEntry = matM(d1d2,d3);
 
-		cacheM[lookup] = newEntry;
-		return newEntry;
-	} 
+    cacheM[lookup] = newEntry;
+    return newEntry;
+  } 
 }
 
 cacheMain = {};
 transform3Into2_NEW = function(x,y,z, cx,cy,cz, th) {
-	var lookup = ""+x+y+z+cx+cy+cz+th.y;
-	var existingEntry = cacheMain[lookup];
-	if (existingEntry !== undefined) {
-		return existingEntry;
-	}
-	else {
-		var d_1x2x3 = getM(th);
+  var lookup = ""+x+y+z+cx+cy+cz+th.y;
+  var existingEntry = cacheMain[lookup];
+  if (existingEntry !== undefined) {
+    return existingEntry;
+  }
+  else {
+    var d_1x2x3 = getM(th);
 
-		var d4 = [
-			x,
-			y,
-			z
-		];
-		var d5 = [
-			cx,
-			cy,
-			cz
-		];
+    var d4 = [
+      x,
+      y,
+      z
+    ];
+    var d5 = [
+      cx,
+      cy,
+      cz
+    ];
 
-		var d4subd5 = matS(d4,d5);
-		var d = matMV(d_1x2x3, d4subd5);
-		var b = getB(d[0],d[1],d[2]);
-		cacheMain[lookup] = b;
-		return b;
-	}
+    var d4subd5 = matS(d4,d5);
+    var d = matMV(d_1x2x3, d4subd5);
+    var b = getB(d[0],d[1],d[2]);
+    cacheMain[lookup] = b;
+    return b;
+  }
 }
