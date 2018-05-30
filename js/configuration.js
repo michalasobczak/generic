48e8fb6c9c5e16fe97abd5dd85950ad05938a27f
Michal Sobczak <michalasobczak@gmail.com>
2018-05-30 17:36:18 +0200

    Sylvester, configuration

---
+++ js/configuration.js
@@ -0,0 +1,72 @@
+exports = {}
+zoom = 1;
+cacheRectsTimes = [];
+draws = 0;
+cached = 0;
+notCached = 0;
+
+// //////////////
+// CANVAS
+// //////////////
+var ctx = undefined;
+var ctxSb = undefined;
+// MAX DIMS
+var maxW = parseInt($(window).width());
+var maxH = parseInt($(window).height());
+// HOLDERS
+var w = 320;
+var wClear = 320;
+var wStep = w/1000;
+var h = 212;
+var hClear = 212;
+var hStep = h/1000;
+// SCALE
+var scaleW;
+var scaleH;
+var translateByX;
+// RELATIVE DIMS
+var canvasWidth;
+var canvasHeight;
+var canvasWidthClear;
+var canvasHeightClear;
+var canvasWidthHalf;
+var canvasHeightHalf;
+
+// //////////////
+// GENERAL
+// //////////////
+var paused = false;
+var profiler;
+var buttons = [];
+var accReading = Vector3(0,0,0);
+var accReadingPrevious = Vector3(0,0,0);
+var accCounter = 0;
+var vertexCounter = 0;
+var sky;
+var audioController;
+var statusBar;
+var player;
+var collisionDetector;
+var enemyManager;
+var queue = {};
+
+// //////////////
+// DATA
+// //////////////
+var map;
+var models = [];
+var modelsCounter = 0;
+var enemies = [];
+var textureCache = {};
+
+// //////////////
+// CAMERA
+// //////////////
+var camera;
+var occlusionCulling;
+var sign = 1;
+
+// //////////////
+// LIGHT SOURCE
+// //////////////
+var lightSource;
