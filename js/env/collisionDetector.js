14ebc9c1dc99ead6ab986b53ba503ae3e17f172e
Michal Sobczak <michalasobczak@gmail.com>
2018-05-30 17:30:51 +0200

    Simple Collision Detector

---
+++ js/env/collisionDetector.js
@@ -0,0 +1,24 @@
+var CollisionDetector = function() {
+  profiler.log("CollisionDetector - new");
+  return {
+    checkCollisions: function() {
+      for (var index=0; index<=models.length-1; index++) {
+        currentModel = models[index];
+
+        if (currentModel.isCollidable()) {
+          var dist = getDistance3(camera.getPosition(), currentModel.getPosition());
+          var radius1 = currentModel.size;
+          var radius2 = camera.size;
+          var radiusSum = radius1 + radius2;
+          if (dist <= radiusSum) {
+            profiler.log("COLL CAM W/ " + currentModel.modelRef);
+            //models[index].setColor("#FF0000");
+          }
+          else {
+            //models[index].setColor("#63C6FF");
+          }
+        }
+      }
+    }
+  };
+};
