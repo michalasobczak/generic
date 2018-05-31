ed0caf949bc99bb7ccdbec4a13e6289cab2207d1
Michal Sobczak <michalasobczak@gmail.com>
2018-05-31 10:19:40 +0200

    Formatting

--- js/core.js
+++ js/core.js
@@ -3,42 +3,32 @@
 // RUN
 // //////////////
 // //////////////
-
 var signA = 1;
-
+//
 function run() {
   profiler.start();
-  
   // UPDATE && DRAW  
   if (paused == false && ctx != undefined) {
     // CLEAR
     clearCanvas();
-    
-    //try {
-      // OCCLUSION CULLING
-      occlusionCulling.update();
-
-      for (var mindex=0; mindex<=occlusionCulling.sortArray.length-1; mindex++) {    
-        midSorted = occlusionCulling.sortArray[mindex].modelID;
-        model = models[midSorted];              
-        if (model != null && model != undefined && model.loaded) {        
-          model.drawObject();
-        }
-      }  
-    //}  
-    //catch (err) {
-    //  profiler.log("DRAW ERR => " + err.message);
-    //}
-
+    // OCCLUSION CULLING
+    occlusionCulling.update();
+	  //
+    for (var mindex=0; mindex<=occlusionCulling.sortArray.length-1; mindex++) {    
+      midSorted = occlusionCulling.sortArray[mindex].modelID;
+      model = models[midSorted];              
+      if (model != null && model != undefined && model.loaded) {        
+        model.drawObject();
+      }
+    } // for
     // COLLISIONS
     //collisionDetector.checkCollisions();
-
     // CAMERA
     camera.update();    
-  }
-    
+  } // if !paused
+  //
   profiler.finish();  
-}
+} // run
 
 
 // //////////////
@@ -50,24 +40,15 @@ $(document).ready(function() {
   // LOGGER
   profiler = Profiler();
   profiler.log("Logger created");
-
-
   // INITIALIZERS #1
   initializeCanvas();      
-
-
   // CANVAS
   profiler.log("Creating canvas context...");  
   var _canvas_ = document.getElementById("theCanvas");
   _canvas_.style.border = "1px solid silver";
-    var _statusBar_ = document.getElementById("statusBar");
   ctx = _canvas_.getContext("2d");
-    ctxSb = _statusBar_.getContext("2d");
   ctx.scale(scaleW, scaleH);
-    ctxSb.scale(scaleW, scaleH);
   profiler.log("Canvas created");  
-  
-  
   // INIT
   camera = Camera();
   occlusionCulling = OcclusionCulling();  
@@ -75,24 +56,27 @@ $(document).ready(function() {
   lightSource.position = camera.getOrigin();
   map = Map();    
   collisionDetector = CollisionDetector();  
-
-  
   // PC KEYBOARD
   if (getBrowserType() == GENERAL) {
     camera.updateDirection();
-
+	  //
     $("#theCanvas").mousedown(function(e) {
       if (e.which == 1) {
+        ;
       }
       else if (e.which == 3) {
         e.preventDefault();
       }
-    });
+    }); // mousedown
+    //
     $("#theCanvas").click(function(e) {
-    });
+      ;
+    }); // click
+    //
     $("body").bind('contextmenu', function(e){
       return false;
-    }); 
+    }); //contextmenu
+    //
     $("body").keypress(function(e) {      
       //profiler.log("Pressed " + e.keyCode);
       if (e.keyCode == '13') {
@@ -102,7 +86,7 @@ $(document).ready(function() {
         else {
           paused = true;
         }
-      }
+      } // 13
       else if (e.keyCode == '119') {
         profiler.log("FWD");
         try {
@@ -111,26 +95,24 @@ $(document).ready(function() {
         catch (err) {
           profiler.log("FWD => " + err.message);
         }
-      }
+      } // FWD 119
       else if (e.keyCode == '115') {
         profiler.log("BCK");
         camera.moveBackward();
-      }
+      } // BCK 115
       else if (e.keyCode == '97') {
         profiler.log("LFT");
         camera.turnLeft();
         camera.noTurn();
-      }
+      } // LFT 97
       else if (e.keyCode == '100') {
         profiler.log("RGT");
         camera.turnRight();
         camera.noTurn();
-      }
-
-    });
-  }
-
-  
+      } // RGT 100
+    }); // keypress
+  } // if browser GENERAL
+  //
   try {
     // MAP && MODELS    
     map.create();    
@@ -140,38 +122,37 @@ $(document).ready(function() {
     map.updatePathFindingGrid();    
     // CAMERA INIT
     camera.calcRotPos();
-  }
+  } // try
   catch (err) {
     profiler.log("CORE ERR = > " + err.message);
-  }
-
-  
+  } // catch
+  //
+  // MODEL AT 0 POSITON
   models[0].initTexture("itest");
-  //models[1].initTexture("itest");
+  // SAMPLE ROTATION
   models[3].rotate(-Math.PI/32, Vector3(0,0,1));
-
-  
+  //
+  // TODO: verify if still needed
   setInterval(function() {
     if (paused == false) {
-      //doSteer();
+      ;
     }
   }, 100);
-
+  //
   // COLLECTIONS  
   setInterval(function() {
     try {
       map.collectVisibles();
-    }
+    } // try
     catch (err) {
       profiler.log("COLLECT: " + err.message);
-    }
-  }, 300);
-
-
+    } // catch
+  }, 300); // setInterval
+  //
   // ////
   // RUN
   // ////
   profiler.log("About to set RUNNER...");    
-
+  //
   setInterval(run, camera.refreshRate);
-});
+}); // document ready
